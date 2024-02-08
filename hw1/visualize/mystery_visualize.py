622b5a0be72747d2360d0dd3ca6f1ab403cc8e22
kyumly <skkim3360@naver.com>
2024-02-08 19:53:05 +0900

    visualize write

--- hw1/visualize/mystery_visualize.py
+++ hw1/visualize/mystery_visualize.py
@@ -6,8 +6,22 @@ import matplotlib.pyplot as plt
 import cv2
 import pdb
 
+def griad(X):
+    fig = plt.figure(figsize=(10, 10))
+    #X=np.log(X+1)
+    k = X[:, :, 8]
+    for i in range(0, 9):
+        ax = fig.add_subplot(3, 3, i+1)
+        ax.imshow(X[:, :, i],cmap="plasma" )  # 이미지를 표시
+    plt.show()
 
-def colormapArray(X, colors):
+
+def grid_save(X):
+    for i in range(9):
+       print(i)
+       plt.imsave(f"./result{name}/vis_{i}.png" ,X[:,:,i],cmap="plasma")
+
+def colormapArray(X, colors, name=0):
     """
     Basically plt.imsave but return a matrix instead
 
@@ -17,11 +31,8 @@ def colormapArray(X, colors):
     Outputs:
         a HxW uint8 image using the given colormap. See the Bewares
     """
-    print(X.shape)
-    print(colors.shape)
-    for i in range(9):
-        plt.imsave("vis_%d.png" % i,X[:,:,i])
-
+    griad(X)
+    #grid_save(X)
 
     return None
 
@@ -29,6 +40,8 @@ def colormapArray(X, colors):
 if __name__ == "__main__":
     colors = np.load("mysterydata/colors.npy")
     data = np.load("mysterydata/mysterydata.npy")
+    data = np.load("mysterydata/mysterydata2.npy")
+    name = 1
 
-    colormapArray(data, colors)
-    pdb.set_trace()
+    #pdb.set_trace()
+    colormapArray(data, colors, name)
