c54d78457dcd725ddc19f85ffdb4faf548b79e7d
kyumly <skkim3360@naver.com>
2024-03-03 15:09:11 +0900

    테스트 작성중

--- hw1/visualize/mystery_visualize.py
+++ hw1/visualize/mystery_visualize.py
@@ -7,6 +7,14 @@ import cv2
 import pdb
 
 
+"""
+docker, git-flow 
+tool : torch, tensorflows
+"""
+
+
+
+
 def data_preprocessing(image):
     max = np.nanmean(image, axis=(0,1))
     min = np.nanstd(image, axis=(0,1))
@@ -24,9 +32,12 @@ def print_matInfo(name, image):
         image = image[0, 0,:]
     else:
         image = image[0,0]
-    if image.dtype == 'uint8':     mat_type = "CV_8U"
-    elif image.dtype == 'uint32':     mat_type = "CV_32U"
-    elif image.dtype == 'int8':    mat_type = "CV_8S"
+    if image.dtype == 'uint8':
+        mat_type = "CV_8U"
+    elif image.dtype == 'uint32':
+        mat_type = "CV_32U"
+    elif image.dtype == 'int8':
+        mat_type = "CV_8S"
     elif image.dtype == 'uint16':  mat_type = "CV_16U"
     elif image.dtype == 'int16':   mat_type = "CV_16S"
     elif image.dtype == 'float32': mat_type = "CV_32F"
@@ -67,33 +78,83 @@ def colormapArray(X, colors):
     #     ax.imshow(X[:,:,i], camp='gray')
     # plt.imshow()
 
-    # X = data_preprocessing(X)
-    #
-    # x_mean, x_std =cv2.meanStdDev(X)
-    # print_matInfo("X", X)
-    #
-    # print(f"평균값 {x_mean.reshape(-1)} \n"
-    #       f" 분산값 : {x_std.reshape(-1)}")
+    #X = data_preprocessing(X)
 
-    X = np.log1p(X)
-    X = data_preprocessing(X)
+    x_mean, x_std =cv2.meanStdDev(X)
+    print_matInfo("X", X)
 
-
-    xt_mean, xt_std = cv2.meanStdDev(X)
-    print_matInfo("X transform", X)
-    print(f"평균값 {xt_mean.reshape(1, -1)} \n"
-          f" 분산값 : {xt_std.reshape(1, -1)}")
-
-    print()
-
+    print(f"평균값 {x_mean.reshape(-1)} \n"
+          f" 분산값 : {x_std.reshape(-1)}")
     grid(X)
 
     return None
 
 
+def get_spectrum(colors):
+    plt.imshow(colors[:, 0].reshape(1, -1), cmap='viridis', aspect='auto')
+    plt.colorbar()
+    plt.title('Colormap Array')
+    plt.xlabel('Value')
+    plt.ylabel('Index')
+    plt.show()
+
+
 if __name__ == "__main__":
     colors = np.load("mysterydata/colors.npy")
-    data = np.load("mysterydata/mysterydata3.npy")
+    data = np.load("mysterydata/mysterydata4.npy")
 
-    colormapArray(data, colors)
+    print(colors.shape)
+    data = data.astype(np.uint8)
+
+    # 플로팅
+    #get_spectrum(colors)
+
+    ex = data[:, :, 0]
+    # channels = cv2.split(data)
+
+    # print(colors[:, 0])
+    #colors[:,0], colors[:,2] =colors[:,2],colors[:,0]
+    # print(colors[:, 2])
+
+    # for channel in channels:
+    #     merge_list = []
+    #     #ex_normalize = cv2.normalize(channel, None, 0, 255, cv2.NORM_MINMAX)
+    #     for color in colors.T:
+    #         """
+    #         R, G , B 순서
+    #         """
+    #         colors_image = cv2.LUT(channel, color)
+    #         merge_list.append(colors_image)
+    #
+    #     break
+
+    # colors_image =np.stack(merge_list, axis=2)
+
+    #print(colors_image[0][200])
+    #print(colors_image[0].max(), colors_image[0].min())
+    # print(channels[0].shape)
+    # print(colors.T[0:1, :].shape)
+
+    r = colors.T[0:1, :][:, ex]
+    g = colors.T[1:2, :][:, ex]
+    b = colors.T[2:3, :][:, ex]
+
+
+    colors_image = r[0]
+
+    plt.imshow(ex)
+    plt.title('original Image')
+    plt.axis('off')
+    plt.show()
+
+    plt.imshow(colors_image)
+    print(colors_image)
+    plt.title('Colored Image')
+    plt.axis('off')
+    plt.show()
+
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    #colormapArray(data, colors)
     #pdb.set_trace()
